e4ff7b9da3a7c187445ebd3362414a8be8b94744
BABAAKIHISA <is0632vv@ed.ritsumei.ac.jp>
2026-03-09 10:00:47 +0900

    Update to My S3Bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,18 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { storage } from './storage/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
-  storage,
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: "ap-southeast-2",
+    bucket_name: "recoding-upload-baba",
+  },
 });
