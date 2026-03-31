a8b0277a2c39ae8c7ca19500f96e046f373816c7
BABAAKIHISA <is0632vv@ed.ritsumei.ac.jp>
2026-03-31 12:03:57 +0900

    bag syusei

--- src/components/AudioList.jsx
+++ src/components/AudioList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { generateClient } from 'aws-amplify/data';
 import WaveSurfer from 'wavesurfer.js';
 
